aa707049426eec0eb253ce1a711676048a914e16
vytro <brayan.bbgc@gmail.com>
2023-11-26 22:12:48 -0400

    added if for data consistency

--- src/main/java/com/diplomado/courses/repositories/StudentIdCardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.diplomado.courses.repositories;
-
-import com.diplomado.courses.domain.entities.StudentIdCard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentIdCardRepository extends JpaRepository<StudentIdCard, Long>{
-}
--- src/main/java/com/diplomado/courses/services/mapper/CustomMapper.java
+++
@@ -1,7 +0,0 @@
-package com.diplomado.courses.services.mapper;
-
-public interface CustomMapper <DTO, E>{
-    E toEntity(DTO dto);
-    DTO toDto(E e);
-    DTO toDtoDetailed(E e);
-}
