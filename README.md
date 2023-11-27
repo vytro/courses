56810633a01914f22f34d2f1c1c7b9b6a617fc7c
vytro <brayan.bbgc@gmail.com>
2023-11-27 16:33:08 -0400

    added flyway

---
+++ README.md
@@ -0,0 +1,8 @@
+# To run the database, use these db parameters.
+    url: jdbc:postgresql://localhost:5432/dev_test
+    username: postgres
+    password: 1234
+# Also create database:
+- dev_test
+- qa_test
+- prod_test
---
+++ src/main/resources/db/migration/V2__createTable_enrollment_and_studentIdCard.sql
@@ -0,0 +1,36 @@
+
+-- create enrollment table
+create table enrollment(
+                           id bigint not null,
+                           grade integer not null,
+                           created_at timestamp not null,
+                           student_id integer,
+                           course_id integer,
+                           primary key (id)
+);
+
+create sequence enrollment_sequence as integer increment 1;
+
+alter table enrollment add constraint FK_Enrollment_Ref_Student foreign key (student_id)
+    references student (id) on delete restrict on update restrict;
+
+alter table enrollment add constraint FK_Enrollment_Ref_Course foreign key (course_id)
+    references course (id) on delete restrict on update restrict;
+
+--add new column
+ALTER TABLE book ADD COLUMN created_at timestamp;
+
+
+-- brand dto
+
+create table student_id_card(
+                                id bigint not null,
+                                card_number varchar(100) not null,
+                                student_id int,
+                                primary key (id)
+);
+
+alter table student_id_card add constraint fk_student_id_card_ref_student foreign key (student_id)
+    references student (id) on delete restrict on update restrict;
+
+create sequence my_sequence as integer increment 1;
